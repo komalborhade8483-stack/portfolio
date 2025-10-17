7ed8e613101da6cda1eceb9778d6ae73b91a138d
Komal Borhade <komalborhade8483@gmail.com>
2025-10-18 00:52:25 +0530

    added routing with three pages (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import Navbar from "./comps/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./comps/Home";
+import Projects from "./comps/Projects";
+import Aboutme from "./comps/Aboutme";
+import urls from "./url";
 
 function App() {
-  return <Navbar></Navbar>;
+  return <>
+    <Navbar></Navbar>
+
+    <Routes>
+      <Route path={urls.home} element={<Home></Home>}></Route>
+      <Route path={urls.about_me} element={<Aboutme></Aboutme>}></Route>
+      <Route path={urls.projects} element={<Projects></Projects>}></Route>
+    </Routes>
+  </>;
 }
 
 export default App;
